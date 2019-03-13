91af8b44bd68faf8a257460c2b697ff174bd320f
jonathan shaner <technicalcookie@gmail.com>
2019-03-13 15:35:21 -0700

    update

--- jobs/app.py
+++ jobs/app.py
@@ -18,10 +18,10 @@ def execute_sql(sql, values=(), commit=False, single=False):
     if commit == True:
         results = connection.commit()
     else:
-        results = cursor.fetchone () if single else cursor.fetchall.()
+        results = cursor.fetchone() if single else cursor.fetchall()
 
     cursor.close()
-    return results
+    return result
 
 @app.teardown_appcontext
 
